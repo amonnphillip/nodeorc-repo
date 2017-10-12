7868f6c3d52597e35d75ff7191ae15655d0ca032
amonn phillip <amonnphillip@gmail.com>
2017-10-12 13:13:43 -0700

    experiment with ffi to get gpgme functionality

--- src/gpg/gpg.ts
+++ src/gpg/gpg.ts
@@ -4,12 +4,45 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as GpgLib from 'gpg';
 import * as childProcess from 'child_process';
+import * as ffi from 'ffi';
+import * as ref from 'ref';
+
+const types = {
+  REGSAM: ref.types.ulong,
+  DWORD: ref.types.uint32,
+  ULONG: ref.types.ulong,
+  HWND: ref.refType(ref.types.void),
+  BYTE: ref.types.uint8,
+  HKEY: ref.refType(ref.types.void),
+  PVOID: ref.refType('pointer'),
+  HANDLE: ref.refType(ref.types.void),
+  HINSTANCE: ref.refType(ref.types.void),
+  LPCTSTR: ref.refType(ref.types.CString),
+  STRING: ref.types.CString,
+  INT: ref.types.int,
+  LPVOID: ref.refType(ref.types.void),
+  PHKEY: null,
+  LPBYTE: null,
+  LPDWORD: null,
+};
+
+types.PHKEY = ref.refType(types.HKEY);
+types.LPBYTE = ref.refType(types.BYTE);
+types.LPDWORD = ref.refType(types.DWORD);
 
 export class Gpg {
   private gpgUser: 'NodeOrc';
   private destinationPath: string;
   constructor(private config: Config) {
     this.destinationPath = path.resolve('./gpg/');
+
+    const intPtr = ref.refType('int');
+    const libm = ffi.Library('F:\\Program Files (x86)\\GnuPG\\bin\\libgpgme-11', {
+      gpgme_free: ['void', [types.LPVOID]],
+    });
+
+    const err = libm.gpgme_new(intPtr);
+    console.log(err);
   }
   async createKeys(): Promise<any> {
     const file = path.resolve('./gpg/gpggen.txt');
